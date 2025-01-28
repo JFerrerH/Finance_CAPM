2e49be42e694f19266639f637b7b9f80424ea8c5
JFerrerH <132410631+JFerrerH@users.noreply.github.com>
2025-01-27 20:06:08 -0500

    feature: add monthly returns graph

--- app.py
+++ app.py
@@ -46,15 +46,7 @@ data_indice['Monthly_Return_Index'] = data_indice["Close"].pct_change()
 data = data_accion.join(data_indice['Monthly_Return_Index']).dropna()
 
 # Display Data
-tabs = st.tabs(["CAPM Model","Regression Analysis","Stock Data", "Market Data"])
-
-with tabs[2]:
-    st.write("### Stock Data")
-    st.dataframe(data_accion[['Close', 'Monthly_Return_Stock']].dropna())
-
-with tabs[3]:
-    st.write("### Market Index Data")
-    st.dataframe(data_indice[['Close', 'Monthly_Return_Index']].dropna())
+tabs = st.tabs(["CAPM Model","Monthly Returns","Regression Analysis","Stock Data", "Market Data"])
 
 # Calculate correlation and regression
 correlation = data['Monthly_Return_Stock'].corr(data['Monthly_Return_Index'])
@@ -64,20 +56,6 @@ lm = sm.OLS(Y, X).fit()
 intercept, beta_daily_return_indice = lm.params
 
 y_pred = beta_daily_return_indice * X['Monthly_Return_Index'] + intercept
-
-with tabs[1]:
-    st.write("### Regression Analysis")
-    st.write(f"Beta (β) of {ticker_accion}: {beta_daily_return_indice:.2f}")
-    st.write(f"Correlation: {correlation:.2f}")
-    
-    # Scatterplot
-    fig, ax = plt.subplots(figsize=(8,5))
-    sns.scatterplot(x=X['Monthly_Return_Index'], y=Y, ax=ax)
-    sns.lineplot(x=X['Monthly_Return_Index'], y=y_pred, color='red', ax=ax)
-    plt.xlabel("Market Monthly Return")
-    plt.ylabel("Stock Monthly Return")
-    plt.title("Regression Analysis: Beta Calculation")
-    st.pyplot(fig)
 
 # CAPM Calculation
 bond_ticker = st.sidebar.text_input("Enter Bond Ticker (e.g., ^TNX)", "^TNX")
@@ -110,5 +88,44 @@ with tabs[0]:
     plt.legend()
     plt.grid(False)
     st.pyplot(fig)
+
+with tabs[1]:
+    st.write("### Monthly Returns Stock vs Index")
+    fig, ax = plt.subplots(figsize=(8,4))
+    sns.lineplot(data=data, x=data.index, y='Monthly_Return_Stock', label='Monthly Return Stock')
+    sns.lineplot(data=data, x=data.index, y='Monthly_Return_Index', label='Monthly Return Index')
+    plt.gca().spines[['top', 'right']].set_visible(False)
+    plt.xlabel("Fecha")
+    plt.ylabel("Retorno Mensual")
+    plt.title("Monthly Returns Comparison")
+    plt.legend()
+    st.pyplot(fig)
+
+with tabs[2]:
+    st.write("### Regression Analysis")
+    st.write(f"Beta (β) of {ticker_accion}: {beta_daily_return_indice:.2f}")
+    st.write(f"Correlation: {correlation:.2f}")
+    
+    # Scatterplot
+    fig, ax = plt.subplots(figsize=(8,5))
+    sns.scatterplot(x=X['Monthly_Return_Index'], y=Y, ax=ax)
+    sns.lineplot(x=X['Monthly_Return_Index'], y=y_pred, color='red', ax=ax)
+    plt.xlabel("Market Monthly Return")
+    plt.ylabel("Stock Monthly Return")
+    plt.title("Regression Analysis: Beta Calculation")
+    st.pyplot(fig)
+
+with tabs[3]:
+    st.write("### Stock Data")
+    st.dataframe(data_accion[['Close', 'Monthly_Return_Stock']].dropna())
+
+with tabs[4]:
+    st.write("### Market Index Data")
+    st.dataframe(data_indice[['Close', 'Monthly_Return_Index']].dropna())
+
+
+
+
+
 
 
